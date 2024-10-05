b7b975ba6d5588591fc42c807bd5f90ccdc7b02f
dmiv <dmitriy40647274@gmail.com>
2024-10-05 17:43:23 +0300

    наметка таблицы

--- frontend/components/directory/LoaderTable.vue
+++ frontend/components/directory/LoaderTable.vue
@@ -5,6 +5,7 @@
         <th>Марка</th>
         <th>Номер</th>
         <th>Грузоподъемность</th>
+        <th>Активен</th>
         <th>Действия</th>
       </tr>
     </thead>
@@ -13,6 +14,18 @@
         <td>{{ loader.brand }}</td>
         <td>{{ loader.number }}</td>
         <td>{{ loader.capacity }}</td>
+        <td>
+          <UIcon
+              name="mi:check"
+              v-if="loader.active"
+              class="text-green-700"
+            />
+          <UIcon
+            name="mi:close"
+            class="text-red-700"
+            v-else
+            />
+        </td>
         <td>
           <button @click="$emit('edit', loader)">Изменить</button>
           <button @click="$emit('delete', loader)">Удалить</button>
--- frontend/pages/directory/[entity].vue
+++ frontend/pages/directory/[entity].vue
@@ -7,16 +7,25 @@ const directory = ref({
 if (entity == 'loaders') {
   directory.value.name = 'Погрузчиков'
 }
+const Loaders = [
+  {
+    id: "1", brand: "Амкадор", number: "45-65 PH-1", capacity: "2.5", active: true,
+  },
+  {
+    id: "2", brand: "Амкадор", number: "45-66 PH-1", capacity: "2.6", active: false,
+  },
 
+]
 </script>
 
 <template>
   <h1 class="font-bold text-2xl">Справочник {{directory.name}}</h1>
   <DirectorySearch />
+  <DirectoryLoaderTable :loaders="Loaders" />
 </template>
 
 <style scoped>
-.h1 {
-  margin-bottom: 15px;
+h1 {
+  margin-bottom: 10px;
 }
 </style>
