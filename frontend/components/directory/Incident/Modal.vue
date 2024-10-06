5a613edbf3edc99c2ca209fbe5e21c3d8123a9a8
dmiv <dmitriy40647274@gmail.com>
2024-10-06 12:24:30 +0300

    modal v-model

---
+++ frontend/components/directory/Incident/Modal.vue
@@ -0,0 +1,76 @@
+<script setup lang="ts">
+import {format} from "date-fns";
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: null
+  },
+  isActive: {
+    type: Boolean,
+    default: false
+  }
+});
+const isActive = computed({
+  get: () => props.isActive,
+  set: (value) => {
+    emit('close')
+  }
+})
+const emit = defineEmits(['update:model-value', 'close', 'save']);
+</script>
+
+<template>
+  <UModal v-model="isActive">
+    <div class="p-4 bg-gray-200 rounded-2xl text-gray-500 font-semibold">
+      <div class="modal_header text-center">
+        Проблемы с погрузчиком? Опишите
+      </div>
+      <div class="modal_divider bg-red-700 h-0.5 w-full my-2 mb-3"></div>
+
+      <div class="modal_date_container flex flex-row mb-4">
+        <div class="modal_date_start">
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            Начало
+            <UButton :label="format(props.modelValue.dateStart, 'dd.MM.yyyy HH:mm')"
+                     class="ml-4 mr-4 bg-white text-black"/>
+            <template #panel="{ close }">
+              <DatePicker v-model="props.modelValue.dateStart" is-required @close="close"/>
+            </template>
+          </UPopover>
+        </div>
+        <div class="modal_date_end">
+          <UPopover :popper="{ placement: 'bottom-start' }">
+            Окончание
+            <UButton v-if="props.modelValue.dateEnd" :label="format(props.modelValue.dateEnd, 'dd.MM.yyyy HH:mm')"
+                     class="ml-4 mr-4 bg-white text-black"/>
+            <UButton v-else label="еще активен" class="ml-4 mr-4 bg-white text-black"/>
+            <template #panel="{ close }">
+              <DatePicker v-model="props.modelValue.dateEnd" nullable @close="close"/>
+            </template>
+          </UPopover>
+        </div>
+      </div>
+      <div class="content mb-4 flex flex-col gap-2">
+        описание инцидента
+        <textarea cols="50" rows="5" class="rounded-xl" v-model="props.modelValue.description"></textarea>
+      </div>
+      <div class="modal_buttons flex justify-center gap-10">
+        <UButton
+            class="w-32 flex justify-center items-center bg-red-700"
+            label="Сохранить"
+            @click="emit('save')"
+        />
+        <UButton
+            class="w-32 flex justify-center items-center bg-gray-500 text-black"
+            label="Выход"
+            @click="emit('close')"
+        />
+      </div>
+    </div>
+  </UModal>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/components/directory/Incident/Page.vue
+++ frontend/components/directory/Incident/Page.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-defineProps(['loaderId'])
+const props = defineProps(['loaderId'])
 const loader = ref({
   number: 1,
   incidentList: [
@@ -18,15 +18,20 @@ const loader = ref({
 
   ]
 })
-import {format} from 'date-fns'
-
 const isModalActive = ref(false)
 const addRow = () => {
   isModalActive.value = true
-  // loader.value.incidentList.push({creation: true})
 }
-const dateStart = ref(new Date())
-const dateEnd = ref(null)
+const incidentData = ref({
+  number: loader.number,
+  dateStart: new Date(),
+  dateEnd: null,
+  description: '',
+})
+const saveData = () => {
+  console.log('process', incidentData.value)
+  isModalActive.value = false
+}
 </script>
 
 <template>
@@ -39,53 +44,10 @@ const dateEnd = ref(null)
           @click="addRow"
       >Добавить
       </UButton>
+      <DirectoryIncidentModal v-model="incidentData" :is-active="isModalActive"
+                              @close="isModalActive = false" @save="saveData" />
       <DirectoryIncidentTable :incidents="loader.incidentList"/>
-      <UModal v-model="isModalActive">
-        <div class="p-4 bg-gray-200 rounded-2xl text-gray-500 font-semibold">
-          <div class="modal_header text-center">
-            Проблемы с погрузчиком? Опишите
-          </div>
-          <div class="modal_divider bg-red-700 h-0.5 w-full my-2 mb-3"></div>
 
-          <div class="modal_date_container flex flex-row mb-4">
-            <div class="modal_date_start">
-              <UPopover :popper="{ placement: 'bottom-start' }">
-                Начало
-                <UButton :label="format(dateStart, 'dd.MM.yyyy HH:MM')" class="ml-4 mr-4 bg-white text-black"/>
-                <template #panel="{ close }">
-                  <DatePicker v-model="dateStart" is-required @close="close"/>
-                </template>
-              </UPopover>
-            </div>
-            <div class="modal_date_end">
-              <UPopover :popper="{ placement: 'bottom-start' }">
-                Окончание
-                <UButton v-if="dateEnd" :label="format(dateEnd, 'dd.MM.yyyy HH:MM')"
-                         class="ml-4 mr-4 bg-white text-black"/>
-                <UButton v-else label="еще активен" class="ml-4 mr-4 bg-white text-black"/>
-                <template #panel="{ close }">
-                  <DatePicker v-model="dateEnd" nullable @close="close"/>
-                </template>
-              </UPopover>
-            </div>
-          </div>
-          <div class="content mb-4 flex flex-col gap-2">
-            описание инцидента
-            <textarea cols="50" rows="5" class="rounded-xl"></textarea>
-          </div>
-          <div class="modal_buttons flex justify-center gap-10">
-            <UButton
-                class="w-32 flex justify-center items-center
-                 bg-red-700"
-                label="Сохранить"
-            />
-            <UButton
-                class="w-32 flex justify-center items-center bg-gray-500 text-black"
-                label="Выход"
-            />
-          </div>
-        </div>
-      </UModal>
     </div>
   </slot>
   <slot v-else></slot>
