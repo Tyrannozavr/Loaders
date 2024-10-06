315901a982222acc8921c935977143bb457f5b77
dmiv <dmitriy40647274@gmail.com>
2024-10-06 23:15:02 +0300

    сохранение корректно

--- backend/loaders/admin.py
+++ backend/loaders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Loader, Brand, Incidents
 
-# Register your models here.
+admin.site.register(Loader)
+admin.site.register(Brand)
+admin.site.register(Incidents)
--- backend/loaders/views.py
+++ backend/loaders/views.py
@@ -1,13 +1,33 @@
 # views.py
-from rest_framework import generics, serializers
+from rest_framework import generics, serializers, status
 from rest_framework.permissions import IsAuthenticated
-from .models import Loader
+from rest_framework.response import Response
+
+from .models import Loader, Brand
 from .serializers import LoaderSerializer
 
 class LoaderListCreateView(generics.ListCreateAPIView):
     queryset = Loader.objects.all()
     serializer_class = LoaderSerializer
     permission_classes = [IsAuthenticated]  # Require authentication
+
+    def create(self, request, *args, **kwargs):
+        # Perform any custom processing here
+        # For example, you might want to validate or modify the incoming data
+        data = request.data
+        data['brand'] = Brand.objects.get_or_create(name=data['brand'])[0].id
+        data['updated_by'] = request.user.id
+
+        # Now use the modified data to create the object
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)  # Validate the data
+
+        # Save the new instance
+        self.perform_create(serializer)
+
+        # Return the response with the created object
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class LoaderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Loader.objects.all()
