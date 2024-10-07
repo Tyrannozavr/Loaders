cc3e2fbf6874c1351c84e15640ab4aaa1d5bc8a2
dmiv <dmitriy40647274@gmail.com>
2024-10-07 18:01:14 +0300

    prod

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,6 +130,14 @@ DATABASES = {
         'PORT': '5432',  # default PostgreSQL port
     }
 }
+
+STATIC_URL = 'collected_static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / 'collected_static'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
